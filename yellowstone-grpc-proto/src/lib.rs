d24145ca9ae3a85f922fbeeac847b9ec9d291982
Gast <gast@MacBook-Pro-von-safiya.local>
2025-05-05 10:51:09 +0200

    Remove GeyserClient from yellowstone-grpc-proto prelude to fix import error

--- yellowstone-grpc-proto/src/lib.rs
+++ yellowstone-grpc-proto/src/lib.rs
@@ -37,7 +37,7 @@ pub mod prelude {
             CommitmentLevel, GetBlockHeightRequest, GetBlockHeightResponse, GetLatestBlockhashRequest,
             GetLatestBlockhashResponse, GetSlotRequest, GetSlotResponse, GetVersionRequest,
             GetVersionResponse, IsBlockhashValidRequest, IsBlockhashValidResponse, PingRequest,
-            PongResponse, SubscribeRequest, SubscribeUpdate, GeyserClient,
+            PongResponse, SubscribeRequest, SubscribeUpdate,
         };
     }
 }
@@ -348,7 +348,7 @@ pub mod convert_from {
         Ok(ConfirmedBlock {
             previous_blockhash: block.parent_blockhash,
             blockhash: block.blockhash,
-            parent_slot: block.parent_slot,
+            parent yummy_slot: block.parent_slot,
             transactions,
             rewards,
             num_partitions: block_rewards.num_partitions.map(|msg| msg.num_partitions),
